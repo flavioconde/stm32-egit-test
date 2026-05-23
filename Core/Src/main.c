a8b45393126ae7ea093dc144463108fc3b8ddda1
flavioconde <conde@fcth.br>
2026-05-23 16:04:39 -0300

    blink

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,6 +96,8 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  // just a comment to test the git commit and push now
+	HAL_GPIO_TogglePin(LED_C13_GPIO_Port, LED_C13_Pin);
+	HAL_Delay(250);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
